43acee0fa94bd7d58cd88f42f1517812b26d88a5
Hitman69420 <87095287+Hitman69420@users.noreply.github.com>
2023-07-29 19:52:29 +0530

    Add files via upload

---
+++ Web_app/pages/2_YOLO_webrtc.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+from yolo_predictions import YOLO_Pred
+from streamlit_webrtc import webrtc_streamer
+import av
+
+yolo = YOLO_Pred(onnx_model='./model/best.onnx',data_yaml='./model/data.yaml')
+
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+    pred_img = yolo.predictions(img)
+    return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
+
+
+webrtc_streamer(key="example", video_frame_callback=video_frame_callback,media_stream_constraints={"video":True,"audio":False})
